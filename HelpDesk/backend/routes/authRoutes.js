a1a23ac461a1546adec566242e7d2166a096b8dc
Md1rsZambrano <mdzambrano0321@utm.edu.ec>
2026-07-29 17:15:57 -0500

    feat: backend API resuelto de forma limpia

--- HelpDesk/backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Rutas de autenticación
-router.post('/register', authController.registrar);
-router.post('/login', authController.login);
-
-module.exports = router;
